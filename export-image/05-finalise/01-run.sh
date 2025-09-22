dd7580703ce3f8320a640ff66fe5a84725ba77a9
cuu <gnubsd@me.com>
2025-09-23 00:47:47 +0800

    update

--- export-image/05-finalise/01-run.sh
+++ export-image/05-finalise/01-run.sh
@@ -71,7 +71,7 @@ owner_id=$(stat -c '%u' "$d")
 mkdir -p "$d/.config"
 cp -rf files/user/.* "$d/" || echo "cp failed"
 chown -R $owner_id "$d/.config"
-
+chown -R $owner_id "$d/.lexaloffle"
 
 cp -rf files/dphys-swapfile "${ROOTFS_DIR}/etc/" || echo "cp dphys-swapfile failed"
 
@@ -84,7 +84,7 @@ cp -rf files/cursors "${ROOTFS_DIR}/usr/share/icons/PiXflat/"
 cp -rf files/chromium-browser.desktop  "${ROOTFS_DIR}/usr/share/applications/"
 
 
-on_chroot << EOF
+on_chroot << EOF1
 
 rm /home/cpi/dphys-swapfile
 rm /home/cpi/x.pkla
@@ -95,7 +95,123 @@ sed -i 's/^#\?HandlePowerKey=.*/HandlePowerKey=ignore/' /etc/systemd/logind.conf
 
 systemctl set-default multi-user.target
 
+
+tee /etc/systemd/system/save_backlight.service <<'EOF'
+
+[Unit]
+Description=Run sync_backlight.sh on shutdown
+DefaultDependencies=no
+Before=shutdown.target reboot.target halt.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/save_backlight.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=shutdown.target reboot.target halt.target
+
 EOF
+
+systemctl daemon-reload
+
+systemctl enable save_backlight.service
+
+tee /etc/systemd/system/sync_backlight.service <<'EOF'
+
+[Unit]
+Description=Sync LCD backlight at boot
+After=multi-user.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/local/bin/sync_backlight.sh
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+systemctl daemon-reload
+systemctl enable sync_backlight.service
+
+tee /usr/local/bin/sync_backlight.sh <<'EOF'
+#!/bin/sh
+
+SYNC_FILE="/usr/local/etc/sync_backlight"
+BRIGHTNESS_PATH="/sys/class/backlight/picocalc_lcd_backlight/brightness"
+
+mkdir -p /usr/local/etc
+
+if [ -f "\$SYNC_FILE" ]; then
+    VALUE=\$(cat "\$SYNC_FILE")
+    echo "\$VALUE" > "\$BRIGHTNESS_PATH"
+else
+
+    echo 30 > "\$BRIGHTNESS_PATH"
+    echo 30 > "\$SYNC_FILE"
+fi
+EOF
+
+chmod +x /usr/local/bin/sync_backlight.sh 
+
+tee /usr/local/bin/save_backlight.sh <<'EOF'
+#!/bin/sh
+
+SYNC_FILE="/usr/local/etc/sync_backlight"
+BRIGHTNESS_PATH="/sys/class/backlight/picocalc_lcd_backlight/actual_brightness"
+
+VALUE=\$(cat "\$BRIGHTNESS_PATH")
+
+echo "\$VALUE" > "\$SYNC_FILE"
+EOF
+
+chmod +x /usr/local/bin/save_backlight.sh
+
+tee /usr/local/bin/toggle_dpms.sh <<'EOF'
+#!/bin/bash
+
+status=\$(cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor)
+
+if [ "\$status" = "powersave" ]; then
+    xset dpms force on
+    sleep 1
+    echo "DPMS On"
+else
+    xset dpms force off
+    sleep 1
+    echo "DPMS Off"
+fi
+EOF
+
+chmod +x /usr/local/bin/toggle_dpms.sh
+
+tee /etc/motd <<'EOF'
+PicoCalc Zero
+Image Version: v1.0 (Build 2025-09-22)
+
+Default Login:
+  Username: cpi
+  Password: cpi
+  SSH is enabled. Please change your password before going online.
+
+Mouse Mode:
+  Press [Right SHIFT] to toggle mouse mode.
+  In mouse mode:
+    Arrow keys move the cursor
+    Key [ / ] act as left/right mouse buttons
+
+GUI Mode:
+  Run \`startx\` to launch the desktop environment.
+
+Happy Hacking!
+clockworkpi.com
+EOF
+
+sed -i 's/quiet splash/ /g' /boot/cmdline.txt
+
+EOF1
 
 update_issue "$(basename "${EXPORT_DIR}")"
 install -m 644 "${ROOTFS_DIR}/etc/rpi-issue" "${ROOTFS_DIR}/boot/issue.txt"
--- export-image/05-finalise/files/user/.bashrc
+++ export-image/05-finalise/files/user/.bashrc
@@ -43,7 +43,7 @@ esac
 # uncomment for a colored prompt, if the terminal has the capability; turned
 # off by default to not distract the user: the focus in a terminal window
 # should be on the output of commands, not on the prompt
-force_color_prompt=yes
+#force_color_prompt=yes
 
 if [ -n "$force_color_prompt" ]; then
     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
@@ -112,3 +112,4 @@ if ! shopt -oq posix; then
   fi
 fi
 
+export LC_ALL=en_US.UTF-8
